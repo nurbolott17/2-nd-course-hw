907f751d02c119db2762d3e5e5f3d3a7518996f7
nurbolott17 <nurbolot.nurbolot2002@mail.ru>
2025-03-04 17:51:55 +0300

    ДО РАБОТАЛ ЗАДАЧУ №5

--- hw8.js
+++ hw8.js
@@ -1,72 +1,72 @@
-// Задание 1
+// // Задание 1
+
+// // const people = [
+// //    { name: 'Глеб', age: 29 },
+// //    { name: 'Анна', age: 17 },
+// //    { name: 'Олег', age: 7 },
+// //    { name: 'Оксана', age: 47 }
+// // ];
+
+
+// // console.log(people.sort((a,b) => a.age - b.age));
+
+// // Задание 2
+
+
+// function isPositive(num) {
+//     return num > 0
+//  }
+// function isMale(person) {
+//     return person.gender === "male";
+// }
+// function filter(arr,ruleFunction) {
+//     const a =[];
+//     for (let i = 0; i < arr.length; i++) {
+//        if (ruleFunction(arr[i])){
+//             a.push(arr[i])
+//        }
+//     }
+//     return a
+// }
+
+// console.log(filter([3, -4, 1, 9], isPositive));
 
 // const people = [
-//    { name: 'Глеб', age: 29 },
-//    { name: 'Анна', age: 17 },
-//    { name: 'Олег', age: 7 },
-//    { name: 'Оксана', age: 47 }
+//    {name: 'Глеб', gender: 'male'},
+//    {name: 'Анна', gender: 'female'},
+//    {name: 'Олег', gender: 'male'},
+//    {name: 'Оксана', gender: 'female'}
 // ];
 
-
-// console.log(people.sort((a,b) => a.age - b.age));
-
-// Задание 2
+// console.log(filter(people, isMale));
 
 
-function isPositive(num) {
-    return num > 0
- }
-function isMale(person) {
-    return person.gender === "male";
-}
-function filter(arr,ruleFunction) {
-    const a =[];
-    for (let i = 0; i < arr.length; i++) {
-       if (ruleFunction(arr[i])){
-            a.push(arr[i])
-       }
-    }
-    return a
-}
+// // Задание 3
+// // Напишите программу, которая на протяжении 30 секунд каждые 3 секунды будет выводить в консоль текущую дату. Последней строкой должно выводиться сообщение «30 секунд прошло».
+// const intervalid = setInterval(() => {
+//     console.log('hello')
+// }, 3000);
 
-console.log(filter([3, -4, 1, 9], isPositive));
+// setTimeout(() => {
+//     clearInterval(intervalid);
+//     console.log('Время прошло');
 
-const people = [
-   {name: 'Глеб', gender: 'male'},
-   {name: 'Анна', gender: 'female'},
-   {name: 'Олег', gender: 'male'},
-   {name: 'Оксана', gender: 'female'}
-];
-
-console.log(filter(people, isMale));
+// }, 7000);
 
 
-// Задание 3
-// Напишите программу, которая на протяжении 30 секунд каждые 3 секунды будет выводить в консоль текущую дату. Последней строкой должно выводиться сообщение «30 секунд прошло».
-const intervalid = setInterval(() => {
-    console.log('hello')
-}, 3000);
+// // Задание 4
+// // Сейчас код ниже выводит в консоль «Привет, Глеб!» сразу после запуска.
 
-setTimeout(() => {
-    clearInterval(intervalid);
-    console.log('Время прошло');
+// //
+// // .
 
-}, 7000);
+// function delayForSecond(callback) {
+//    setTimeout(callback, 2000);
+// }
 
-
-// Задание 4
-// Сейчас код ниже выводит в консоль «Привет, Глеб!» сразу после запуска.
-
-//
-// .
-
-function delayForSecond(callback) {
-   setTimeout(callback, 2000);
-}
-
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+// delayForSecond(function () {
+//    console.log('Привет, Глеб!');
+// })
 
 
 
@@ -87,4 +87,4 @@ function sayHi (name) {
 // // Код выше менять нельзя
 
 
-delayForSecond(sayHi('Глеб'))
+delayForSecond(() => sayHi('Глеб'))
