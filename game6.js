acd049c17454e0a784dd3ece1bf48867c987c498
nurbolott17 <nurbolot.nurbolot2002@mail.ru>
2025-03-25 13:47:07 +0300

    Курсовая

--- game6.js
+++ game6.js
@@ -9,5 +9,21 @@ btnEl.addEventListener('click', function() {
     sectionGamesEl.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         
 }); 
-    
-    
+
+
+
+const topBtnEl = document.querySelector('.top__btn');
+const blockGamesEl = document.getElementById('blockGames')
+const gamesEl = document.getElementById('games')
+topBtnEl.addEventListener('click', function (){
+    if (blockGamesEl) {
+        element = document.getElementById("blockGames");
+        element.scrollIntoView(true);
+    } else if (gamesEl){
+        element = document.getElementById("games") 
+        element.scrollIntoView(true);
+    }
+});
+ 
+
+   
